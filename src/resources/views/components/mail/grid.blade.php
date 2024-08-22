838ed21d78920696cf2ffc2fa7ff38d5b00219b6
Jérémy Lemesle <Korko@users.noreply.github.com>
2024-08-22 16:52:47 +0200

    Allow grid slots to have attributes

--- src/resources/views/components/mail/grid.blade.php
+++ src/resources/views/components/mail/grid.blade.php
@@ -8,21 +8,21 @@
                 <td align="center" valign="top">
                     <table {{ $attributes->merge(['class' => "container grid grid-col-$columns"]) }} width="600" cellpadding="0" cellspacing="0" border="0">
                         <tr>
-                            <td class="mobile" align="center" valign="top">
+                            <td class="mobile" align="center" valign="top" {{ $one?->attributes }}>
                                 {{ $one ?: $slot }}
                             </td>
                             @if ($columns >= 2)
-                                <td class="mobile" align="center" valign="top">
+                                <td class="mobile" align="center" valign="top" {{ $two->attributes }}>
                                     {{ $two }}
                                 </td>
                             @endif
                             @if ($columns >= 3)
-                                <td class="mobile" align="center" valign="top">
+                                <td class="mobile" align="center" valign="top" {{ $three->attributes }}>
                                     {{ $three }}
                                 </td>
                             @endif
                             @if ($columns >= 4)
-                                <td class="mobile" align="center" valign="top">
+                                <td class="mobile" align="center" valign="top" {{ $four->attributes }}>
                                     {{ $four }}
                                 </td>
                             @endisset
