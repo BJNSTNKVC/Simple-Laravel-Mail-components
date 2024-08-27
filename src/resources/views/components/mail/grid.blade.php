1fb3df9ec626d3599e2e0afbf12678a6c61c3c58
Jérémy Lemesle <Korko@users.noreply.github.com>
2024-08-27 20:58:19 +0200

    Slots attributes with merge class

--- src/resources/views/components/mail/grid.blade.php
+++ src/resources/views/components/mail/grid.blade.php
@@ -8,21 +8,21 @@
                 <td align="center" valign="top">
                     <table {{ $attributes->merge(['class' => "container grid grid-col-$columns"]) }} width="600" cellpadding="0" cellspacing="0" border="0">
                         <tr>
-                            <td class="mobile" align="center" valign="top" {{ $one?->attributes }}>
+                            <td {{ $one?->attributes->merge(['class' => "mobile"]) }} align="center" valign="top">
                                 {{ $one ?: $slot }}
                             </td>
                             @if ($columns >= 2)
-                                <td class="mobile" align="center" valign="top" {{ $two->attributes }}>
+                                <td {{ $two->attributes->merge(['class' => "mobile"]) }} align="center" valign="top">
                                     {{ $two }}
                                 </td>
                             @endif
                             @if ($columns >= 3)
-                                <td class="mobile" align="center" valign="top" {{ $three->attributes }}>
+                                <td {{ $three->attributes->merge(['class' => "mobile"]) }} align="center" valign="top">
                                     {{ $three }}
                                 </td>
                             @endif
                             @if ($columns >= 4)
-                                <td class="mobile" align="center" valign="top" {{ $four->attributes }}>
+                                <td {{ $four->attributes->merge(['class' => "mobile"]) }} align="center" valign="top">
                                     {{ $four }}
                                 </td>
                             @endisset
